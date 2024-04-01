8783e271a1edc48710d86dda8a6e61f21e2be95a
soban <muhammad.soban@digitify>
2024-04-01 20:44:05 +0500

    add

---
+++ index.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env/node
+import inquirer from "inquirer";
+const answers =await inquirer.prompt([
+    {
+        name:"userguess",
+        type:"number",
+        message:"Enter a number between 1 and 10"
+    }
+
+])
+const randomNumber=Math.floor(Math.random() * 3);
+console.log(randomNumber);
+console.log(answers.userguess);
+if (randomNumber!==answers.userguess) {
+    console.log("wrong guess");
+}else{
+    console.log("correct guess");
+}
